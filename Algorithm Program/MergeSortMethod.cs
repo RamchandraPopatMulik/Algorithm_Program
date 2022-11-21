21f434152a650f1a72ae7a3f9084176c7afde8c4
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-11-21 16:28:40 +0530

    Added file

---
+++ Algorithm Program/MergeSortMethod.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Program
+{
+    public class MergeSortMethod
+    {
+        public void merge(int[]array,int left,int mid,int right)
+        {
+            int sizeOfArray1=mid-left+1;
+            int sizeOfArray2=right-mid;
+            int[]tempLeft=new int[sizeOfArray1];
+            int[]tempRight=new int[sizeOfArray2];
+            int i, j;
+
+            for(i=0;i<sizeOfArray1;++i)
+               tempLeft[i]= array[left + i];
+            for (j = 0; j < sizeOfArray2; ++j)
+                tempRight[j] = array[mid + 1+j];
+            i = 0;
+            j= 0;
+            int k=left;
+            while(i<sizeOfArray1 && j<sizeOfArray2)
+            {
+                if (tempLeft[i] <= tempRight[j])
+                {
+                    array[k]= array[i];
+                    i++;
+                }
+                else
+                {
+                    array[k]= array[j];
+                    j++;
+                }
+                k++;
+            }
+            while(i< sizeOfArray1)
+            {
+                array[k]=tempLeft[i];
+                i++;
+                k++;
+            }
+            while (j < sizeOfArray2)
+            {
+                array[k] = tempLeft[j];
+                j++;
+                k++;
+            }
+        }
+        public void Sort(int[]array,int left,int right)
+        {
+            if (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                Sort(array, left, mid);
+                Sort(array, mid + 1, right);
+                merge(array, left, mid, right);
+            }
+        }
+        public void Display(int[] array)
+        {
+            int n = array.Length;
+            for (int i = 0; i < n; i++)
+            Console.Write(array[i] + "  ");
+        }
+    }
+}
--- Algorithm Program/Program.cs
+++ Algorithm Program/Program.cs
@@ -5,12 +5,12 @@
         static void Main(string[] args)
         {
             int [] array = {51,56,9,565,6,32,57,36,78,35,26,65};
-            InsertionSortMethod insertionSortMethod = new InsertionSortMethod();
+            MergeSortMethod insertionSortMethod = new MergeSortMethod();
             Console.WriteLine("Before Sorting Element are :");
             insertionSortMethod.Display(array);
 
-            insertionSortMethod.sort(array);
-            Console.WriteLine("After Sorting Elements are :");
+            insertionSortMethod.Sort(array,0,array.Length-1);
+            Console.WriteLine("\nAfter Sorting Elements are :");
             insertionSortMethod.Display(array);
             Console.ReadLine();
         }
