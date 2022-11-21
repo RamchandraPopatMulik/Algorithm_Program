ed72b640f546e98edf8362197afcdc91851d2840
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-11-21 12:53:53 +0530

    Added file

---
+++ Algorithm Program/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Algorithm_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Input input = new Input();
+            input.UserInput();
+        }
+    }
+}
