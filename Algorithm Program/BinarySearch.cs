10f15712a474415735f5563d001f228aac28d364
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-11-21 15:49:30 +0530

    Added file

--- Algorithm Program/BinarySearch.cs
+++ Algorithm Program/BinarySearch.cs
@@ -7,34 +7,28 @@ using System.Threading.Tasks;
 
 namespace Algorithm_Program
 {
-    public class BinarySearch
+    public class InsertionSortMethod
     {
-        public string filepath = @"E:\Basic Core Program\Algorithm Program\Algorithm_Program\Algorithm Program\BinarySearchData.json";
-
-        public void BinarySearchWordCheck()
+        public void sort(IComparable[] array)
         {
-            string wordsdata = "BMW,TataSafari,TataNano,Jaguar,Swift,Suzuki,Nexon,Honda,Mahindra,RangeRover";
-            File.WriteAllText(filepath, JsonConvert.SerializeObject(wordsdata));
-
-            string wordData = File.ReadAllText(filepath);
-            string word = JsonConvert.DeserializeObject<string>(wordData);
-            List<string> wordList = word.Split(',').ToList<string>();
-            wordList.Sort();
-
-            Console.WriteLine("Enter a Word to Search: ");
-            string wordToSearch = Console.ReadLine();
-
-            int index = wordList.BinarySearch(wordToSearch);
-
-            if (index < 0)
+            int index;
+            for(int i=1;i<array.Length;i++)
             {
-                Console.WriteLine($"{wordToSearch} does not exist");
+                IComparable word=array[i];
+                index = i - 1;
+                while((index>=0 )&& (array[index].CompareTo(word)>0))
+                {
+                    array[index+1] = array[index];
+                    index--;
+                }
+                array[index + 1] = word;
             }
-            else
-            {
-                Console.WriteLine($"{wordToSearch} exists");
-            }
-
+        }
+        public void Display(IComparable[] array)
+        {
+            int n = array.Length;
+            for (int i = 0; i < n; i++)
+            Console.Write(array[i] + "  ");
         }
     }
 }
--- Algorithm Program/Program.cs
+++ Algorithm Program/Program.cs
@@ -4,8 +4,15 @@
     {
         static void Main(string[] args)
         {
-            BinarySearch binarySearchData = new BinarySearch();
-            binarySearchData.BinarySearchWordCheck();
+            string [] names = {"Ram", "Sachin","Rahul","Mahesh","Shrikant","Sanjiv","Sourabh","Mohan","Sandip"};
+            InsertionSortMethod insertionSortMethod = new InsertionSortMethod();
+            Console.WriteLine("Before Sorting Element are :");
+            insertionSortMethod.Display(names);
+
+            insertionSortMethod.sort(names);
+            Console.WriteLine("After Sorting Elements are :");
+            insertionSortMethod.Display(names);
+            Console.ReadLine();
         }
     }
 }
