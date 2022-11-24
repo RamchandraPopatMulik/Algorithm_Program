5828c0cc603dd3604d9df0169b18b2b860b07bff
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-11-24 12:54:51 +0530

    Added file

---
+++ Algorithm Program/PrimeNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Program
+{
+    public class PrimeNumber
+    {
+        public void CheckPrimeNumber(int start,int end)
+        {
+            int count = 0;
+            for(int i=start;i<=end;i++)
+            {
+                bool flag=false;
+                for(int j=2;j<=i/2;j++)
+                {
+                    if(i%j==0)
+                    {
+                        flag=true;
+                        break;
+                    }
+                }
+                if(!flag && i!=1)
+                {
+                    Console.WriteLine("{0}",i);
+                    count++;
+                }
+            }
+            Console.WriteLine($"Total Prime Numbers from {start} to {end } are {count}");
+        }
+    }
+}
--- Algorithm Program/Program.cs
+++ Algorithm Program/Program.cs
@@ -4,8 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Anagram anagram = new Anagram();
-            anagram.AnagramDetectionProgram();
+            PrimeNumber  primeNumber = new PrimeNumber();
+            primeNumber.CheckPrimeNumber(1,1000);
         }
     }
 }
